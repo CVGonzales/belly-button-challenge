075fa0b7433709ae368292015b7270d35b9c5ee3
CVGonzales <carlv.gonzales@gmail.com>
2022-11-10 10:46:36 +1100

    fixed bar chart, added bubble chart to app.js

--- static/js/app.js
+++ static/js/app.js
@@ -27,24 +27,54 @@ d3.json(url).then(function(data) {
 
 function barChart(sample) {
 
-  let first10Otu = sample.otu_ids.slice(0,10).map((object)=>"OTU "+object).reverse();
-  let first10Labels = sample.otu_labels.slice(0,10).reverse();
-  let first10Values= sample.sample_values.slice(0,10);
-  first10Values.sort((a,b) => a-b);
+  let top10_otu_ids = sample.otu_ids.slice(0,10).map((object)=>"OTU "+object).reverse();
+  let top10_otu_labels = sample.otu_labels.slice(0,10).reverse();
+  let top10_sample_values= sample.sample_values.slice(0,10);
+  top10_sample_values.sort((a,b) => a-b);
 
   let plotData = [{
-    x: first10Values,
-    y: first10Otu,
-    text: first10Labels,
+    x: top10_sample_values,
+    y: top10_otu_ids,
+    text: top10_otu_labels,
     orientation:'h',
     type:"bar"}];
 
   let layout = {
-    height: 500,
-    width: 500
+    height: 600,
+    width: 600
     }; 
   config={responsive:true};
 
   graphDiv = document.getElementById('bar');
   Plotly.newPlot("bar", plotData, layout,config);
+}
+
+// Create a bubble chart that displays each sample.
+// Use otu_ids for the x values.
+// Use sample_values for the y values.
+// Use sample_values for the marker size.
+// Use otu_ids for the marker colors.
+// Use otu_labels for the text values.
+
+function bubbleChart(sample) {
+
+  let bubbleChartData = [{
+    x: sample.otu_ids,
+    y: sample.sample_values,
+    mode:"markers",
+    marker: {
+      size: sample.sample_values,
+      color: sample.otu_ids
+    },
+    text:sample.otu_labels,
+    type:"scatter"
+  }];
+
+  layout = {
+    showlegend: false
+  }
+  config={responsive:true};
+  
+  graphDiv = document.getElementById('bubble');
+  Plotly.newPlot(graphDiv, bubbleChartData, layout,config);
 }
